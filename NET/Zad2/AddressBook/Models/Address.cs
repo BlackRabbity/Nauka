cc1d568b447815c36722b666cb67b886c5a34b79
Michal <michal332111@o2.pl>
2021-03-15 11:42:42 +0100

    Dodano zadanie 2

---
+++ NET/Zad2/AddressBook/Models/Address.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.Models
+{
+    public class Address
+
+    {
+        [Display(Name = "Twoja ulubiona ulica")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "Proszę podać poprawną ulicę"), Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string Street { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string ZipCode { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public string City { get; set; }
+        [Required(ErrorMessage = "Pole jest obowiązkowe")]
+        public int Number { get; set; }
+
+    }
+}
