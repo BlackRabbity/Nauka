89a9a048e68a875d209e24a321e890d7a16f78f2
Michal <michal332111@o2.pl>
2021-04-25 13:28:54 +0200

    zad6

---
+++ NET/Zad6/Zad6_NET/BrowserMiddleware.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Shyjus.BrowserDetection;
+using System.Threading.Tasks;
+
+namespace Zad6_NET.Models
+{
+    public class BrowserDetectionMiddleware
+    {
+        private RequestDelegate _next;
+        public BrowserDetectionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext, IBrowserDetector detector)
+        {
+            var browser = detector.Browser;
+
+            if (browser.Name == BrowserNames.Edge || browser.Name == BrowserNames.EdgeChromium || browser.Name == BrowserNames.InternetExplorer)
+            {
+                await httpContext.Response.WriteAsync("Przegladarka nie jest obslugiwana");
+            }
+            else
+            {
+                await _next.Invoke(httpContext);
+            }
+
+        }
+    }
+    public static class UrlTransformerMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseBrowserDetectionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<BrowserDetectionMiddleware>();
+        }
+    }
+}
