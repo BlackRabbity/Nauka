339d5bc5fe70977e4fcc8a00bca47f045d5fd1d3
Michal <michal332111@o2.pl>
2021-05-16 13:34:00 +0200

    Zad 8

---
+++ NET/Zad8/Zad07_Net/Models/Filter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zad08_Net.Models
+{
+    public class Filter : ResultFilterAttribute
+    {
+        public string ClientIP { get; private set; }
+
+
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            ClientIP = context.HttpContext.Connection.RemoteIpAddress.ToString();
+            var result = (PageResult)context.Result;
+            result.ViewData["IPAddress"] = ClientIP;
+            await next.Invoke();
+        }
+    }
+}
---
+++ NET/Zad8/Zad07_Net/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Zad08_Net.Models;
+
+namespace Zad08_Net.Pages
+{
+    [Filter]
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
